accae55517971be67f7eb51e975e048c18e7725a
yooe <yooe05@gmail.com>
2023-10-09 06:42:05 +0300

    added camera moving and drag and drop for objs

---
+++ Assets/scripts/GrabController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabController : MonoBehaviour
+{
+    private GameObject selectedObj;
+
+    [SerializeField]
+    private float offsetByGround;
+
+    [SerializeField]
+    private Transform marker;
+
+    private Vector3 delta;
+
+    private void Update()
+    {
+
+        //луч засекающий предметы, которые можно перетаскивать
+        RaycastHit dragableHit;
+        Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out dragableHit, 50f, LayerMask.GetMask("dragable"));
+
+        //луч засекающий поверхности, на которые можно ставить предметы
+        RaycastHit groundHit;
+        Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out groundHit, 50f, LayerMask.GetMask("ground"));
+
+        //проверка щелчка лкм
+        if (Input.GetMouseButtonDown(0))
+        {
+            //если пока ничего не перетаскивается
+            if (selectedObj == null)
+            {
+                //и мышь на перетаскиваемом предмете, то захватываем его
+                if (dragableHit.collider)
+                {
+             
+                    dragableHit.rigidbody.isKinematic = true;
+                    selectedObj = dragableHit.collider.gameObject;
+                    selectedObj.transform.rotation = Quaternion.Euler(0, 0, 0); //позже заменить на начальный вектор поворота объекта, т.к. он может быть ненулевым
+
+                    marker.gameObject.SetActive(true);   
+                    delta = selectedObj.transform.position - groundHit.point;
+                }
+
+            }
+            //если что-то уже перетаскивается, то отпускаем его
+            else
+            {
+                selectedObj.GetComponent<Rigidbody>().isKinematic = false;
+                marker.gameObject.SetActive(false);
+                selectedObj = null;
+            }
+        }
+
+        //если захвачен предмет, перемещаем его относительно точки падения луча на поверхность
+        if (selectedObj)
+        {
+            selectedObj.transform.position = new Vector3(groundHit.point.x+ delta.x, groundHit.point.y + offsetByGround, groundHit.point.z+ delta.z);
+
+            RaycastHit hit;
+            Physics.Raycast(selectedObj.transform.position, Vector3.down,out hit, 50f);
+
+            marker.position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+        }
+
+
+
+    }
+
+
+}
